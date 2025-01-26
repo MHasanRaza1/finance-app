7369f661ca73b023895d372cae6892477fc80c0b
mhasanraza1 <muhammadhasanraza45@gmail.com>
2025-01-26 07:29:23 +0500

    main

--- Frontend/src/Api/index.js
+++ Frontend/src/Api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const ApiClient = axios.create({
-    baseURL: "http://localhost:8080/api",
-    timeout: 30000,
+    baseURL: "http://localhost:3000/api",
+    timeout: 10000,
     headers:{
         "Content-Type": "application/json"
     }
